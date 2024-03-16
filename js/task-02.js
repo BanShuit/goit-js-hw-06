fda411e8742e4a1bb8c275a22028260dfb2e3e20
BanShuit <tuna125@o2.pl>
2024-03-16 19:08:41 +0100

    update task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.getElementById('ingredients');
+
+ingredients.forEach(ingredient => {
+  const liElement = document.createElement('li');
+
+  liElement.textContent = ingredient;
+
+  liElement.classList.add('item');
+
+  ulElement.appendChild(liElement);
+});
