33717c05e3f36cde07a008e0702a122bff1a8066
BanShuit <tuna125@o2.pl>
2024-03-19 10:42:34 +0100

    poprawa 7 zadania

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,15 @@
-const rangeInput = document.getElementById('font-size-control');
-const textSpan = document.getElementById('text');
+document.addEventListener('DOMContentLoaded', () => {
+  const rangeInput = document.getElementById('font-size-control');
+  const textSpan = document.getElementById('text');
 
-rangeInput.addEventListener('input', () => {
+
+  function setInitialFontSize() {
     textSpan.style.fontSize = rangeInput.value + 'px';
+  }
+
+  setInitialFontSize();
+
+  rangeInput.addEventListener('input', () => {
+    textSpan.style.fontSize = rangeInput.value + 'px';
+  });
 });
